6d1fd0b14fb1e9575e839ebf40b333f43cd19488
jon-lee1998 <149186400+jon-lee1998@users.noreply.github.com>
2024-01-26 14:58:29 -0500

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from lxml import etree
+import streamlit as st
+
+tree = etree.parse("./xml_example.xml")
+
+lstKey = []
+lstValue = []
+for p in tree.iter() :
+    lstKey.append(tree.getpath(p).replace("/",".")[1:])
+    lstValue.append(p.text)
+
+df = pd.DataFrame({'key' : lstKey, 'value' : lstValue})
+df.sort_values('key')
+st.write(df)
